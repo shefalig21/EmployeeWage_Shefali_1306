521c0b866531a491ad25bbad6e8e3a007e232f9e
shefalig-21 <shefalig4848@gmail.com>
2024-12-04 13:54:25 +0530

    Added USe Case 3

--- UseCases.js
+++ UseCases.js
@@ -12,7 +12,7 @@ if (randomCheck1>0.5) {
 console.log(`Status: ${employeeStatus1}`);
 
 
-//(b)
+//Use Case-2
 
 const randomCheck=Math.random();
 const partTime=4;
@@ -53,6 +53,73 @@ switch(workType)
 }
 
 console.log("Daily Wage: $"+dailyWage);
+
+
+//Use Case-3:
+
+// Use case for Attendance Check
+function checkAttendance() {
+    let employee = Math.random();
+    if (employee < 0.5) {
+        console.log("Absent");
+        return "Absent";    
+    } else {
+        console.log("Present");
+        return "Present";    
+    }
+}
+
+// Use case:Calculate the wages for part-time and full-time
+function calculateDailyWages() {
+    const partTimeHours = 4;
+    const fullTimeHours = 8;
+    const wagesPerHour = 20;
+    
+    let employeeType = Math.floor(Math.random() * 3);
+    let workedHours = 0;
+    
+    switch(employeeType) {
+        case 0:
+            workedHours = 0;
+            console.log("No Time worked");
+            break;
+        case 1:
+            workedHours = partTimeHours;
+            console.log("Part Time Worked");
+            break;
+        case 2:
+            workedHours = fullTimeHours;
+            console.log("Full Time Worked");
+            break;
+        default:
+            workedHours = 0;
+            console.log("Invalid Output");
+    }
+    
+    let dailyWages = workedHours * wagesPerHour;
+    return { dailyWages, workedHours };  // Return
+}
+
+//Use Case: refactor the code and find the work hours 
+
+function checkAttendanceAndCalculateWages() {//function to check attendance and calculate wage
+    let attendanceStatus = checkAttendance();
+    let dailyWages = 0;
+    let workedHours = 0;
+
+    if (attendanceStatus === "Present") {
+        let result = calculateDailyWages();
+        dailyWages = result.dailyWages;
+        workedHours = result.workedHours;
+    } else {
+        console.log("No wages as the employee is absent.");
+    }
+    return { workedHours, dailyWages}; // Return 
+}
+
+let result = checkAttendanceAndCalculateWages();
+console.log("Total worked hours: " + result.workedHours);
+console.log("Final wages: $" + result.dailyWages);
 
 
 
