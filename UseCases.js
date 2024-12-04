98d0925d0b87df760c00e7665461f73a01ac4187
shefalig-21 <shefalig4848@gmail.com>
2024-12-04 13:28:25 +0530

    Added Use Case 2

--- UseCases.js
+++ UseCases.js
@@ -1,12 +1,58 @@
 //Use Case-1:
 
-const randomCheck=Math.random();
-let employeeStatus;
+const randomCheck1=Math.random();
+let employeeStatus1;
 
-if (randomCheck>0.5) {
-    employeeStatus="Present";
+if (randomCheck1>0.5) {
+    employeeStatus1="Present";
 } else{
-    employeeStatus="Absent";
+    employeeStatus1="Absent";
 }
 
-console.log(`Status: ${employeeStatus}`);
+console.log(`Status: ${employeeStatus1}`);
+
+
+//(b)
+
+const randomCheck=Math.random();
+const partTime=4;
+const fullTime=8;
+const perHourWage=20;
+
+let employeeStatus=randomCheck>0.5?"Present":"Absent";
+
+let dailyWage=0;
+
+if(employeeStatus==="Present")
+{
+    console.log("Employee is present");
+
+let workType=Math.floor(Math.random()*3);
+
+switch(workType)
+{
+    case 0:
+        console.log("No Work done");
+        dailyWage=0;
+        break;
+        
+    case 1:
+        console.log("Part Time Employee");
+        dailyWage=perHourWage*partTime;
+        break;
+        
+    case 2:
+        console.log("Full Time employee");
+        dailyWage=perHourWage*fullTime;
+        break;
+        
+  }
+
+} else{
+    console.log("Employee is absent");
+}
+
+console.log("Daily Wage: $"+dailyWage);
+
+
+
