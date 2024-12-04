82959e2bf10e4e7ff3c13d984f5e57fadd1937b8
shefalig-21 <shefalig4848@gmail.com>
2024-12-04 15:01:46 +0530

    Total Monthly wages for 20 Days

--- UseCases.js
+++ UseCases.js
@@ -121,5 +121,79 @@ let result = checkAttendanceAndCalculateWages();
 console.log("Total worked hours: " + result.workedHours);
 console.log("Final wages: $" + result.dailyWages);
 
+//Use Case: 4
+
+
+// Use case for Attendance Check
+function checkAttendance() {
+    let employee = Math.random();
+    if (employee < 0.5) {
+
+      return "Absent";
+    } else {
+      return "Present";
+    }
+  }
+  
+  // Use case: Calculate the wages for part-time and full-time
+  function calculateDailyWages() {
+    const partTimeHours = 4;
+    const fullTimeHours = 8;
+    const wagesPerHour = 20;
+
+    let employeeType = Math.floor(Math.random() * 3);
+    let workedHours = 0;
+  
+    switch (employeeType) {
+      case 0:
+        workedHours = 0;
+      //   console.log("No Time worked");
+        break;
+      case 1:
+        workedHours = partTimeHours;
+      //   console.log("Part Time Worked");
+        break;
+      case 2:
+        workedHours = fullTimeHours;
+      //   console.log("Full Time Worked");
+        break;
+      default:
+        workedHours = 0;
+      //   console.log("Invalid Output");
+    }
+  
+    let dailyWages = workedHours * wagesPerHour;
+    return { dailyWages, workedHours }; 
+  }
+  
+  //Use Case3 refactor the code and find the work hours
+  // Function to check attendance and calculate wages if present
+  let result1 = calculateDailyWages();
+  console.log("Total worked hours: " + result.workedHours);
+  console.log("Final wages: $" + result.dailyWages);
+  
+  //Use case :using for loop
+  function monthlySalary() {
+    let totalHours = 0;
+    let Salary = 0;
+    let totalDays = 0;
+  
+    for (let i = 0; i < 20; i++) {
+      let attendance = checkAttendance();
+      if (attendance) {
+        let result = calculateDailyWages();
+        totalDays++;
+        totalHours += result.workedHours;
+        Salary += result.dailyWages;
+      }
+    }
+    console.log(
+      "The Employee worked hours:",
+      totalHours,
+      "and montly salary:",
+      Salary
+    );
+  }
+  monthlySalary()
 
 
